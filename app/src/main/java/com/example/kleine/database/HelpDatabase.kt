481ee0c28df1bd32a71fb7a99b0897001465f0e6
LAPTOP-LKCOQMQM\user <limeh-wm20@student.tarc.edu.my>
2023-10-02 01:29:36 +0800

    fix bug

--- app/src/main/java/com/example/kleine/database/HelpDatabase.kt
+++ app/src/main/java/com/example/kleine/database/HelpDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [QuizHistory::class, RewardHistory::class, Reward::class], version = 7, exportSchema = false)
+@Database(entities = [QuizHistory::class, RewardHistory::class, Reward::class], version = 8, exportSchema = false)
 abstract class HelpDatabase : RoomDatabase() {
 
     abstract fun quizHistoryDao(): QuizHistoryDao
